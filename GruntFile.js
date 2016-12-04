8ab06ba90536e8d70a90ae67d4fa92cef7395919
Pallegar <pallega@c4b301d5d8eb.ant.amazon.com>
2016-12-05 01:21:48 +0530

    add styles for the main and other components

--- GruntFile.js
+++ GruntFile.js
@@ -6,6 +6,7 @@ module.exports = function(grunt){
 			dist:{
 				files:{
 					'app/styles/main.css':'app/styles/main.scss',
+                    'app/styles/base.css':'app/styles/base.scss',
                     'app/components/headers/header.component.css':'app/styles/header.component.scss',
                     'app/components/toduApp/todu.component.css':'app/styles/todu.component.scss',
                     'app/components/toduApp/insertTodo/inserttodo.component.css':'app/styles/inserttodo.component.scss'
